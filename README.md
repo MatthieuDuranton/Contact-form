38f6c418b355078dbd149967f480a14b6cdc65b7
MatthieuDuranton <matthieu.duranton@hotmail.be>
2019-10-11 16:06:52 +0200

    ajout readme

---
+++ README.md
@@ -0,0 +1,39 @@
+# Exercice Form
+
+### Objectif:
+* Créer une page permettant à des utilisateurs de contacter une entreprise
+* Utiliser PHP
+* Porter le site sur serveur
+* Apprendre à sécuriser la page pour éviter les spams (honeypot)
+* Apprendre à sécuriser les inputs afin de ne pas recevoir de code malicieux (sanitize)
+* Créer une réponse mail
+* Signaler à l'utilisateur les inputs obigatoires
+* Signaler à l'utilisateur les inputs mals remplis
+
+### Contexte :
+* Second mois de la formation
+* format de la page imposé (form, outils...)
+* Moins de 3 jours de travail en mode solo
+
+### Langages et framework :
+* HTML
+* SASS et CSS
+* Bootstrap
+* PHP
+
+### Accès à mes sites :
+* Page Github
+# https://github.com/MatthieuDuranton/Contact-form
+* Page du site
+# https://exercicebecode.000webhostapp.com/ : la page
+
+### Spécificités:
+* Page rendue dynamique
+* Les inputs remplis restent remplis après validation en cas d'erreurs, y compris les menus déroulants et à puces
+* Le reset efface tout les inputs ainsi que les messages d'erreurs
+
+![Mockup](assets/img/mockup.pdf "Mockup page du site poulette")
+
+
+
+
--- index.php
+++ index.php
@@ -4,43 +4,43 @@ $firstname_Err = $lastname_Err = $email_Err = $message_Err = "";
 
                         
 if ($_SERVER['REQUEST_METHOD']=="POST"){
-    
-    if (empty($firstname = htmlspecialchars(strip_tags($_POST['firstname'])))) {
+    if (empty($firstname = htmlspecialchars(strip_tags($_POST['TonPrenom'])))) {
         $firstname_Err = "First name is required";
+        $border_Err;
         }else{
             NULL;
     };
 
-    if (empty($lastname = htmlspecialchars(strip_tags($_POST['lastname'])))) {
+    if (empty($lastname = htmlspecialchars(strip_tags($_POST['TonNom'])))) {
         $lastname_Err = "Last name is required";
         }else{
             NULL;
     };
 
-    $gender = htmlspecialchars(strip_tags($_POST['gender']));
+    $gender = htmlspecialchars(strip_tags($_POST['TonSex']));
 
-    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $email = filter_var($_POST['TonCourrier'], FILTER_SANITIZE_EMAIL);
     
-    $email = $_POST['email'];
+    $email = $_POST['TonCourrier'];
     if (filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
         $email_Err = "is not a valid email address";
     } else {
         NULL;
     };
     
-    $country = htmlspecialchars(strip_tags($_POST['country']));
+    $country = htmlspecialchars(strip_tags($_POST['TonPays']));
 
-    $subject = htmlspecialchars(strip_tags($_POST['subject']));
+    $subject = htmlspecialchars(strip_tags($_POST['TonTheme']));
 
-    if (empty($message = htmlspecialchars(strip_tags($_POST['message'])))) {
+    if (empty($message = htmlspecialchars(strip_tags($_POST['TonBlabla'])))) {
         $message_Err = "You have to provide us with a message";
         }else{
             NULL;
     };
 
-    $gender = $_POST['gender'];
-    $country = $_POST['country'];
-    $subject = $_POST['subject'];
+    $gender = $_POST['TonSex'];
+    $country = $_POST['TonPays'];
+    $subject = $_POST['TonTheme'];
 
     if (isset($_POST['Submitreset'])){
         if ($_POST['Submitreset']=="Reset") {
@@ -48,26 +48,28 @@ if ($_SERVER['REQUEST_METHOD']=="POST"){
             $gender = "Male";
             $country = "Belgium";
             $subject = "other";
-            $firstname_Err = $lastname_Err = $email_Err = $message_Err = "";
+            $firstname_Err = $lastname_Err = $email_Err = $message_Err = $success = $error = "";
         }
     };
     //echo "<pre>";
     //print_r ($_POST);
-    if(isset($_POST["submit"])){
-        if($firstname_Err = $lastname_Err = $email_Err = $message_Err == ""){
-        NULL;
+    
+    /*
+    if(isset($_POST["rendu"])){
+        if($firstname_Err OR $lastname_Err OR $email_Err OR $message_Err == ""){
+            $to = "$email";
+            $subject = 'Request to Hackers Poulette';
+            $reply = 'You succesfully sent your request to our services!';
+            $headers = "From: mduranton63@gmail.com" . "\r\n";
+            if (mail($to, $subject, $reply, $headers)) {
+               return "success";
+            } else {
+               return "error";
+            }
         }else{
-        $subject = $_POST['subject'];
-        $message = $_POST['message'];
-        $headers = 'From:'. $email2 . "Send mail from localhost using PHP"; // Sender's Email
-        $headers .= 'Cc:'. $email2 . "Send mail from localhost using PHP"; // Carbon copy to Sender
-        // Message lines should not exceed 70 characters (PHP rule), so wrap it
-        $message = wordwrap($message, 70);
-        // Send Mail By PHP Mail Function
-        mail($email, $subject, $message, $headers);
-        echo "Your request has been sent successfuly ! Thank you for your feedback";
+            NULL;
         }
-    }
+    }*/
 };
 
 
@@ -96,10 +98,10 @@ echo "</pre>";
         <div class="container">
 
             <div class = "row mt-4 text-center">
-                <div class = "col-1">
+                <div class = "col-12 col-md-1">
                     <a class="nav-link" href="">Home</a>
                 </div>
-                <div class = "col-10">
+                <div class = "col-12 col-md-10">
                     <h1>Feel free to contact us!</h1>
                     <p>Seeking informations, willing to solve a problem or to send love to Johnson's coaches?</p>
                     <p>You just are at the the right place!</p>
@@ -119,50 +121,52 @@ echo "</pre>";
                 </div>
             </div> 
 
-            <form method="POST" action='index.php'>   
+            <form method="POST" action='index.php?'>   
 
                 <div class ="row mt-2 text-center">
-                    <div class ="col-12 text-center">
-                        <label for="firstname">Your first name is:</label>
-                        <input class="form-control" type="text" name="firstname" alt= "indicate your first name" value = <?php echo "$firstname"?>>
+                    <div class ="col-12 col-md-6 text-center">
+                        <label for="TonPrenom">Your first name is:</label>
+                        <input class="form-control" type="text" name="TonPrenom" alt= "indicate your first name" value = <?php echo "$firstname"?>>
                         <span class="error">* <?php echo $firstname_Err;?></span>
                     </div>
 
-                    <div class ="col-12 mt-1 text-center">
-                        <label for="lastname">Your last name is:</label>
-                        <input class="form-control" type="text" name="lastname" alt= "indicate your last name" value = <?php echo "$lastname"?>>
+                    <div class ="col-12 col-md-6 mt-1 text-center">
+                        <label for="TonNom">Your last name is:</label>
+                        <input class="form-control" type="text" name="TonNom" alt= "indicate your last name" value = <?php echo "$lastname"?>>
                         <span class="error">* <?php echo $lastname_Err;?></span>
                     </div>
 
-                    <div class ="col-12 mt-2 text-center">
-                        <label for="gender">Your gender is:</label><br>
-                        <select name="gender" size="1" alt= "choose your gender">
+                    <div class ="col-12 col-md-6 mt-2 text-center">
+                        <label for="TonSex">Your gender is:</label><br>
+                        <select name="TonSex" size="1" class="form-control" alt= "choose your gender">
                             <option value="Male" <?php if (isset($gender) && $gender=="Male") echo "selected";?>>Male</option>
                             <option value="Female" <?php if (isset($gender) && $gender=="Female") echo "selected";?>> Female</option>
                             <option value="Other" <?php if (isset($gender) && $gender=="Other") echo "selected";?>> Other</option>
                         </select>
+                        <span class="error">*</span>
                     </div>
 
-                    <div class ="col-12 mt-2 text-center">
-                    <label for="email">Your e.Mail adress is:</label>
-                        <input class="form-control" type="email" name="email" alt= "indicate your email" value = <?php echo "$email"?>>
+                    <div class ="col-12 col-md-6 mt-2 text-center">
+                    <label for="TonCourrier">Your e.Mail adress is:</label>
+                        <input class="form-control" type="email" name="TonCourrier" alt= "indicate your email" value = <?php echo "$email"?>>
                         <span class="error">* <?php echo $email_Err;?></span>
                     </div>
 
-                    <div class ="col-12 text-center">
-                        <label for="country">Your country is:</label>
-                        <select name="country" size="1" alt= "choose your country">
+                    <div class ="col-12 col-md-6 text-center">
+                        <label for="TonPays">Your country is:</label><br>
+                        <select name="TonPays" size="1" alt= "choose your country" class="form-control">
                             <option value="Belgium" <?php if (isset($country) && $country=="Belgium") echo "selected";?>>Belgium</option>
                             <option value="France" <?php if (isset($country) && $country=="France") echo "selected";?>> France</option>
                             <option value="Luxemburg" <?php if (isset($country) && $country=="Luxemburg") echo "selected";?>> Luxemburg</option>
                             <option value="Switzerland" <?php if (isset($country) && $country=="Switzerland") echo "selected";?>> Switzerland</option>
                             <option value="JohnsonLand" <?php if (isset($country) && $country=="JohnsonLand") echo "selected";?>> JohnsonLand</option>
                         </select>
+                        <span class="error">*</span>
                     </div>
 
-                    <div class ="col-12 mt-1 text-center">
-                        <label for="subject">Subject of your message:</label>
-                        <select name="subject" size="1" alt= "choose your subject">
+                    <div class ="col-12 col-md-6 mt-1 text-center">
+                        <label for="TonTheme">Subject of your message:</label><br>
+                        <select name="TonTheme" size="1" alt= "choose your subject" class="form-control">
                             <option value="Other" <?php if (isset($subject) && $subject=="Other") echo "selected";?>> Other</option>
                             <option value="Emily" <?php if (isset($subject) && $subject=="Emily") echo "selected";?>>About Emily that is soooo great</option>
                             <option value="Marvin" <?php if (isset($subject) && $subject=="Marvin") echo "selected";?>> About Marvin who is soooo intelligent</option>
@@ -172,10 +176,10 @@ echo "</pre>";
                     </div>
 
                     <div class ="col-12 mt-2 text-center">
-                        <label for="message">Your message</label>
+                        <label for="TonBlabla">Your message</label>
                     </div>
                     <div class ="col-12 mt-1 text-center">
-                        <textarea id="message"name="message"rows="5"cols="50" alt= "provide us your message"><?php if(isset($message))echo"$message";?></textarea><br>
+                        <textarea id="TonBlabla"name="TonBlabla"rows="5"cols="50" alt= "provide us your message" class="form-control"><?php if(isset($message))echo"$message";?></textarea><br>
                         <div class="error">* <?php if (isset($message_Err)) echo "$message_Err";?></div>
                     </div>
 
@@ -183,16 +187,30 @@ echo "</pre>";
                         <input type="submit" value="submit">
                     </div>
                     <div class = "col-6 text-center">
+                    <input type= "hidden" name="rendu" value="1">
                         <input type="submit" name="Submitreset" id="Submitreset" value="Reset" />
                     </div>
+                </div>
+                <div>
+                     <!-- H o n e y p o t -->
+                    <label class="flycatcher" for="firstname"></label>
+                    <input class="flycatcher" autocomplete="off" type="text" id="firstname" name="firstname" placeholder="Your firstname here">
+                    <label class="flycatcher" for="lastname"></label>
+                    <input class="flycatcher" autocomplete="off" type="text" id="lastname" name="lastname" placeholder="Your lastname here">
+                    <label class="flycatcher" for="email"></label>
+                    <input class="flycatcher" autocomplete="off" type="email" id="email" name="email" placeholder="Your e-mail here">
+                    <label class="flycatcher" for="subject"></label>
+                    <input class="flycatcher" autocomplete="off" type="text" id="subject" name="subject" placeholder="Your subject here">
+                    <label class="flycatcher" for="message"></label>
+                    <input class="flycatcher" autocomplete="off" type="text" id="message" name="message" placeholder="Your message here">
                 </div>            
             </form>
 
             <footer class="row mt-4">
-                <div class="col-6">                  
+                <div class="col-12 col-md-6">                  
                     <p>Legal disclaimer: By watching this webpage you allow us to use all your datas as we wish. You will be spamed as you've never been before.</p>
                 </div>
-                <div class="col-6">
+                <div class="col-12 col-md-6">
                     <p>This webpage is currently powered by: <a href="https://github.com/MatthieuDuranton" class="btn">Matthieu</a> and is his rightful property. You don't care because you have no use of it anyway</p>                  
                     
                 </div>
@@ -211,4 +229,3 @@ echo "</pre>";
         <script src="assets/js/script.js"></script>
     </body>
 </html>
-
